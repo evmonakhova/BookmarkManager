f175a2ef6a9fdd38b238a336ff83ef2f61aa29ce
e.monakhova <evmonakhova@mail.ru>
2020-05-05 01:52:13 +0300

    Toolbar style added

--- app/src/main/java/monakhova/bookmark/manager/MainActivity.kt
+++ app/src/main/java/monakhova/bookmark/manager/MainActivity.kt
@@ -1,26 +1,23 @@
 package monakhova.bookmark.manager
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_categories, R.id.navigation_add_bookmark, R.id.navigation_settings))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.navigation_categories, R.id.navigation_add_bookmark, R.id.navigation_settings)
+        )
+        toolbar?.setupWithNavController(navController, appBarConfiguration)
+        nav_view.setupWithNavController(navController)
     }
 }
