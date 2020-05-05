41a303422c5d095e45ff317f6123696d5abae85c
e.monakhova <evmonakhova@mail.ru>
2020-05-05 23:03:48 +0300

    CategoriesFragment layout added

--- app/src/main/java/monakhova/bookmark/manager/ui/categories/CategoriesFragment.kt
+++ app/src/main/java/monakhova/bookmark/manager/ui/categories/CategoriesFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import monakhova.bookmark.manager.R
 
@@ -14,18 +12,8 @@ class CategoriesFragment : Fragment() {
 
     private lateinit var categoriesViewModel: CategoriesViewModel
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        categoriesViewModel =
-                ViewModelProviders.of(this).get(CategoriesViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_categories, container, false)
-        val textView: TextView = root.findViewById(R.id.text_categories)
-        categoriesViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        categoriesViewModel = ViewModelProviders.of(this).get(CategoriesViewModel::class.java)
+        return inflater.inflate(R.layout.fragment_categories, container, false)
     }
 }
